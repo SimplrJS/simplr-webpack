8f89682652348930913425c212536ac5680d1985
Martynas Žilinskas <martynas@quatrodev.com>
2018-07-14 05:59:52 +0900

    Types.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+// tslint:disable-next-line:no-reference
+/// <reference path="./types.d.ts" />
+
 export * from "./contracts";
 export * from "./webpack-config";
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,12 @@
 declare module "fork-ts-checker-webpack-plugin";
 
 declare module "write-file-webpack-plugin";
+
+declare module "worker-loader!*" {
+    namespace WorkerLoaderWorker {}
+    class WorkerLoaderWorker extends Worker {
+        constructor();
+    }
+
+    export = WorkerLoaderWorker;
+}
