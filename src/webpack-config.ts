f0e4ca80acf5746b737ad8734586b0612eb81ac6
Martynas Žilinskas <martynas@quatrodev.com>
2018-09-04 18:08:56 +0300

    Added baseHref to HtmlWebpackPlugin.

--- src/webpack-config.ts
+++ src/webpack-config.ts
@@ -120,15 +120,8 @@ export function generateWebpackConfig(opts: SimplrWebpackOptions): Configuration
                       new HtmlWebpackPlugin({
                           inject: false,
                           template: HtmlWebpackTemplate,
+                          baseHref: "/",
                           appMountIds: ["root"],
-                          links: [
-                              // {
-                              //     rel: "stylesheet",
-                              //     href: "https://use.fontawesome.com/releases/v5.0.10/css/all.css",
-                              //     integrity: "sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg",
-                              //     crossorigin: "anonymous"
-                              // }
-                          ],
                           meta: [
                               {
                                   name: "viewport",
